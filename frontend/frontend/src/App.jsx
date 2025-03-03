83d55bf0c787f13f26358c5ed24b5846ed4078c5
Goldigste <daniel.goldbach@tn.techstarter.de>
2025-03-03 19:37:38 +0100

    0.2(Goldi)

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -5,14 +5,15 @@ function App() {
   return (
     <div>
       <h1>To-Do Liste</h1>
-      <input type="text" value= "" onChange= "" placeholder="Neue Aufgabe hinzufügen" />
+      <input id="userInput" type="text" placeholder="Neue Aufgabe hinzufügen" />
       <button onClick="">Hinzufügen</button>
-      <ul id="tasks">
-        <li>Task 1</li>
-        <li>Task 2</li>
-      </ul>
+      <ul id="tasks"><checkDatabase/></ul>
     </div>
   )
 }
+
+// function checkDatabase(){
+//   if 
+// }
 
 export default App;
