c5ead831ac13143efd3d5ee7494af611273e3a5e
JohnnyTec <johnny.hoffmann@tn.techstarter.de>
2025-03-04 12:08:41 +0100

    Revert "Merge branch 'main' of https://github.com/JohhnyTec/ToDoList"
    This reverts commit 7f652c1bb602792eaf7e1c6f6f3e4a634681cc8a, reversing
    changes made to 600c957b8c98eaeab3e5b801cbde5c35583aff7a.

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,6 +1,37 @@
 import React,{ useState, useEffect } from 'react'
 import './App.css'
 
+function CheckDatabase(){
+  const [dbData, setDbData] = useState([]);
+
+  async function getData(){
+    try{
+      const response = await fetch('http://localhost:5000/data');
+      const jsonData = await response.json();
+      setDbData(jsonData.data);
+    } catch (error) {
+    }
+  }
+  useEffect(()=> {
+    getData();
+  }, []);
+  return (
+    <ul>
+      {dbData.map((obj, index) => (
+        <li key={index}>{obj.tasks}</li>
+      ))}
+    </ul>
+  )
+}
+
+function addTask(){
+  let task = document.getElementById('userInput').value;
+  if(task!="") {
+    fetch('http://localhost:5000/add/'+task);
+  }
+  location.reload();
+}
+
 function App() {
   return (
     <div>
